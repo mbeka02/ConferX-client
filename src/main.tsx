4748b9d48134b50ae47e4854e67d0bcaabb699ec
mbeka02 <antonymbeka@gmail.com>
2023-06-26 13:48:14 +0300

    added StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
